0e70deaf678ce4f24907f1d7d9209197ae2e87de
zzkichu118-cpu <zzkichu118@gmail.com>
2026-09-22 20:56:55 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "Custom Status", // required by Discord API, not shown in the client
         state: "N1",     // this is what people actually see
-        type: 3,               // Custom
+        type: 4,               // Custom
       },
     ],
   },
